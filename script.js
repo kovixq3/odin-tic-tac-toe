05d1870b772bc12841e7a7354e79d271a4eae03b
kovixq3 <apkomfposampofpoiasf@gmail.com>
2022-04-20 00:43:18 +0800

    wrap up basic functions

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const Gameboard = (() => {
     let gb = [];
+    const getGb = () => gb;
+
     const item = document.getElementsByClassName('game-board--item');
-    for (let i = 0; i < item.length; i++) {
+    const getGbItem = () => item; 
+
+    const addListeners = () => {
+        for (let i = 0; i < item.length; i++) {
         item[i].dataset.index = i;
-        item[i].addEventListener('click', (event) => Game.gameOneTurn(event))
-        item[i].addEventListener('click', updateDisplay)
-    };
+        item[i].addEventListener('click', function listener(event) {
+            Game.gameOneTurn(event);
+            updateDisplay();
+            item[i].removeEventListener('click', listener);
+        })
+
+
+    }};
+    addListeners();
 
     function updateDisplay() {
         for (let i = 0; i < gb.length; i++) {
@@ -13,65 +24,91 @@ const Gameboard = (() => {
         };
     }
 
-    const gbReset = (() => {
+    const reset = () => {
         gb = [];
         for (let i = 0; i < 9; i++) {
             gb.push('')
         }
-    })();
+        updateDisplay();
+    };
+    reset();
 
-    return { gb }
+    return { getGb, getGbItem, addListeners, reset }
 })();
 
 const Player = (name, marker) => {
-    const placeMarker = (event) => Gameboard.gb[event.target.dataset.index] = marker
+    const placeMarker = (event) => Gameboard.getGb()[event.target.dataset.index] = marker
     const checkWin = () => {
-        console.log('hello shield')
         // horizontal
         for (let x = 0; x <= 8; x += 3) { 
             let y = x + 1;
             let z = x + 2;
-            if (Gameboard.gb[x] !== '' && Gameboard.gb[x] == Gameboard.gb[y] && Gameboard.gb[y] == Gameboard.gb[z]) {
-                console.log('hello flask')
+            if (Gameboard.getGb()[x] !== '' && Gameboard.getGb()[x] == Gameboard.getGb()[y] && Gameboard.getGb()[y] == Gameboard.getGb()[z]) {
+                Game.gameOver(name);
+                return
             }
         }
-
         // vertical
         for (let x = 0; x <= 3; x += 1) { 
             let y = x + 3;
             let z = y + 3;
-            if (Gameboard.gb[x] !== '' && Gameboard.gb[x] == Gameboard.gb[y] && Gameboard.gb[y] == Gameboard.gb[z]) {
-                console.log('hello flask')
+            if (Gameboard.getGb()[x] !== '' && Gameboard.getGb()[x] == Gameboard.getGb()[y] && Gameboard.getGb()[y] == Gameboard.getGb()[z]) {
+                Game.gameOver(name);
+                return
             }
         }
-
         // cross
-        if (Gameboard.gb[2] !== '' && Gameboard.gb[2] == Gameboard.gb[4] && Gameboard.gb[4] == Gameboard.gb[6]) {
-            console.log('hello flask')
-        } else if (Gameboard.gb[0] !== '' && Gameboard.gb[0] == Gameboard.gb[4] && Gameboard.gb[4] == Gameboard.gb[8]) {
-            console.log('hello flask')
+        if (Gameboard.getGb()[2] !== '' && Gameboard.getGb()[2] == Gameboard.getGb()[4] && Gameboard.getGb()[4] == Gameboard.getGb()[6]) {
+            Game.gameOver(name);
+            return
+        } else if (Gameboard.getGb()[0] !== '' && Gameboard.getGb()[0] == Gameboard.getGb()[4] && Gameboard.getGb()[4] == Gameboard.getGb()[8]) {
+            Game.gameOver(name);
+            return
         }
+        // tie
+        if (Game.getTurn() == 9) Game.gameOver('Tie')
     }
+    const getName = () => name
 
-    return { placeMarker, checkWin }
+    return { placeMarker, checkWin, getName }
 }
 const playerOne = Player('wow', 'O')
 const playerTwo = Player('bop', 'X')
 
 const Game = (() => {
     let turn = 0;
+    const getTurn = () => turn;
+
     const gameOneTurn = (event) => {
         turn ++;
         turn % 2 == 1 ? playerOne.placeMarker(event) : playerTwo.placeMarker(event);
         turn % 2 == 1 ? playerOne.checkWin() : playerTwo.checkWin();
-        if (turn == 9) gameover();
     }
-    // new game
-    // gb reset
-    // gameover
-    const gameover = () => {
-        console.log('oof')
-        // a pop up asking if new game
+    const gameOver = (name) => {
+        // remove all listener by clone-ing them and replace them with their clone
+        for (let i = 0; i < Gameboard.getGbItem().length; i++) {
+            let item = Gameboard.getGbItem()[i]
+            let clone = item.cloneNode(true)
+            item.parentNode.replaceChild(clone, item)
+        }
+
+        switch (name) {
+            case playerOne.getName():
+                console.log(`${playerOne.getName()} won!`)
+                break
+            case playerTwo.getName():
+                console.log(`${playerTwo.getName()} won!`)
+                break
+            case 'Tie':
+                console.log('It\'s a Tie!')
+                break
+        }
     }
-    return { gameOneTurn }
+    const newGame = () => {
+        Gameboard.reset();
+        Gameboard.addListeners();
+        turn = 0
+    }
+
+    return { getTurn, gameOneTurn, gameOver, newGame }
 })();
